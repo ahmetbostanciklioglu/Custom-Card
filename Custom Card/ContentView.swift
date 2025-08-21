0458a2ca12babe6407bfbaba3a5fbadeaf60672a
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2025-08-21 23:51:04 +0300

    Initial Commit

---
+++ Custom Card/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Custom Card
+//
+//  Created by Ahmet Bostancıklıoğlu on 21.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Custom Card/Custom_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_CardApp.swift
+//  Custom Card
+//
+//  Created by Ahmet Bostancıklıoğlu on 21.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Custom_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
